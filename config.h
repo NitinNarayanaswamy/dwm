a5bc3b6fdc0535b6e756bfc8f5cdfe9fc6939bb7
NitinNarayanaswamy <nitin.nswamy@gmail.com>
2020-09-30 15:22:08 +0530

    Color theme changed

--- config.h
+++ config.h
@@ -4,21 +4,21 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "DejaVu Sans Mono:size=14" };
 static const char dmenufont[]       = "DejaVu Sans Mono:size=14";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_black[]       = "#293241";
+static const char col_orange[]      = "#EE6C4D";
+/*static const char col_lighterblue[] = "#E0FBFC";*/
+static const char col_lightblue[]   = "#98C1D9";
+/*static const char col_darkblue[]    = "#3D5A80";*/
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_lightblue, col_black, col_black },
+	[SchemeSel]  = { col_orange, col_black, col_orange },
 };
 
 /* tagging */
@@ -58,7 +58,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_black, "-nf", col_lightblue, "-sb", col_black, "-sf", col_orange, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 /* my_commands */
